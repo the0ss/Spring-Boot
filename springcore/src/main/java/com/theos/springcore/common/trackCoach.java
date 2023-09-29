9f30ea154fabbc5fa90b28d772fa51ffca9874fa
the0ss <yuvrajsoorajchauhan@gmail.com>
2023-09-29 11:13:21 +0530

    containers

---
+++ springcore/src/main/java/com/theos/springcore/common/trackCoach.java
@@ -0,0 +1,10 @@
+package com.theos.springcore.common;
+
+public class trackCoach implements Coach {
+
+    @Override
+    public String getdailywork() {
+        return "run for 15 min daily";
+    }
+    
+}
