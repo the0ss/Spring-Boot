f0932185e0fc724f9cde4a9ddb26cb42195a9036
the0ss <yuvrajsoorajchauhan@gmail.com>
2023-10-05 22:04:21 +0530

    form

---
+++ validation-spring-mvc/src/main/java/com/theos/validationspringmvc/ValidationSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package com.theos.validationspringmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidationSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidationSpringMvcApplication.class, args);
+	}
+
+}
---
+++ validation-spring-mvc/src/test/java/com/theos/validationspringmvc/ValidationSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theos.validationspringmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationSpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
