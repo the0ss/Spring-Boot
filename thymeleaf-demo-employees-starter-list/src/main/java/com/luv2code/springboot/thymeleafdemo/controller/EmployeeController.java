047ba5d05b3b2c8312d3da7aed2b56b11e874324
the0ss <yuvrajsoorajchauhan@gmail.com>
2023-10-08 13:52:31 +0530

    CRUD

--- thymeleaf-demo-employees-starter-list/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ thymeleaf-demo-employees-starter-list/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -6,11 +6,17 @@ import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
 import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @Controller
 @RequestMapping("/employees")
@@ -30,8 +36,31 @@ public class EmployeeController {
 		// add to the spring model
 		theModel.addAttribute("employees", theEmployees);
 
-		return "list-employees";
+		return "employees/list-employees";
 	}
+	@GetMapping("/showAddForm")
+	public String add(Model thModel){
+		Employee theEmployee= new Employee();
+		thModel.addAttribute("employee",theEmployee);
+		return "employees/employee-form";
+	}
+	@GetMapping("showUpdateForm")
+	public String update(@RequestParam("employeeId") int theId,Model thModel){
+		Employee thEmployee=employeeService.findById(theId);
+		thModel.addAttribute(thEmployee);
+		return "/employees/employee-form";
+	}
+	@PostMapping("/save")
+	public String save(@ModelAttribute Employee thEmployee) {
+		employeeService.save(thEmployee);
+		return "redirect:/employees/list";
+	}
+	@GetMapping("/delete")
+	public String delete(@RequestParam("employeeId") int id,Model thModel) {
+		employeeService.deleteById(id);
+		return "redirect:/employees/list";
+	}
+	
 }
 
 
